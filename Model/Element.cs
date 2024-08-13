4ea201365ef9c38b2b6ca10de738b180cf4febd2
GFPCommand <49618224+GFPCommand@users.noreply.github.com>
2024-08-13 08:05:17 +0300

    update layout, added Element ctor

--- Model/Element.cs
+++ Model/Element.cs
@@ -1,10 +1,15 @@
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 public class Element
 {
     public string? XPath { get; set; }
     public string? Condition { get; set; }
     public List<string>? Values { get; set; }
+
+    public Element(string xpath, string condition, List<string> values)
+    {
+        XPath = xpath;
+        Condition = condition;
+        Values = values;
+    }
 }
